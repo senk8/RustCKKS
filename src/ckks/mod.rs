4dc1d318ec7fea23971b3ebd866c9b44befc9fc1
佐藤 開智 <revofal@gmail.com>
2021-01-28 22:01:21 +0900

    [UPD]エンコーダの実装途中

--- src/ckks/mod.rs
+++ src/ckks/mod.rs
@@ -1,13 +1,9 @@
 
-/*
-fn CKKSEncoder {
-
+struct CKKSEncoder{
+    m: usize,
+    xi: Compex<f64>
 }
 
-
-struct CKKSContext{
-
-}
 
 impl CKKSContext {
     /*
@@ -20,9 +16,48 @@ impl CKKSContext {
     M = 2N 次元の円分多項式　ΦM(X)=XN+1.
 
     */
-    fn encode(self,plaintext:f64)->
-    {
 
+    /*
+        make vandermode
+        1,1,1,1,1
+        x^1,x^1,x^1,x^1
+    */
+
+    fn vandermonde(root:Complex<f64>)->Vec<Vec<Complex<f64>>>{
+        let N = self.m / 2;
+        let mut mat = vec![];
+
+        for i in 1..N {
+            let root = self.xi.powi(2 * i + 1);
+            let mut row = vec![];
+
+            for j in 1..N {
+                row.push(root.powi(j));
+            }
+
+            mat.push(row)
+        }
+    }
+
+    pub fn encode(self,z:Vec<Complex<f64>>)->Polynomial
+    {
+        let A = self.vandermonde(self.xi);
+        let coeffs = linalg.solve(A,z);
+                
+        Polynomial::new(coeffs)
+    }
+
+    pub fn decode(self,poly: Polynomial)->Vec<Complex<f64>>
+    {
+        let n = self.m / 2;
+        let mut res = vec![];
+
+        for i in 0..n {
+            let root = self.xi.powi(2 * i + 1); 
+            let res = poly.eval(root);
+            res.push(res);
+        }
+
+        res
     }
 }
-*/
